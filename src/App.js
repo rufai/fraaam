d8e0892dbf995bd02a97ab47cf192fb1d3eebc41
Rufai Mustapha <etop94@gmail.com>
2022-01-21 20:30:24 +0100

    package: added framer-motion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { motion } from 'framer-motion';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <motion.p animate={{ opacity:  1 }}>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </motion.p>
         <a
           className="App-link"
           href="https://reactjs.org"
